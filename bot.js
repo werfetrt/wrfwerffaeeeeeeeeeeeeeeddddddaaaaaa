ed7a7d16e159f139342425b6580a9f1d8f4c8183
werfetrt <48763782+werfetrt@users.noreply.github.com>
2019-03-28 22:28:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -226,7 +226,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 250;
+    let amount = 99999999999999999;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
